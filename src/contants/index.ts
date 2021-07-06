2c66780a2d133f601957d73ef90b17bd4326ffc9
than112211 <thannguyenle77@gmail.com>
2021-07-06 15:45:44 +0700

    init project

---
+++ src/contants/index.ts
@@ -0,0 +1,1 @@
+export const HOST_SERVER:string = 'abc'
---
+++ src/pages/mainpage/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from 'components/counter/Counter';
+
+function MainPage() {
+    return (
+        <div className="main">
+            <Counter></Counter>
+        </div>
+    );
+}
+
+export default MainPage;
